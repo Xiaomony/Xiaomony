cf2a2b3f227cfa481e5c859acef1964b49a45122
Xiaomony <117056212+Xiaomony@users.noreply.github.com>
2024-08-19 13:47:31 +0800

    修复小问题

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,11 +1,22 @@
 <template>
     <Hero />
     <myNav crrPage="Intro" />
-    Some Introduce
+    <div class="intro_page">
+        <Container class="intro_content"> Some Introduce </Container>
+    </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+.intro_page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.intro_content {
+    width: 50%;
+}
+</style>
